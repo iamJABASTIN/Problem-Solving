28f13e037825599e4cb109ee80996b836a4a3e70
JABASTIN <itsmejabastin@gmail.com>
2024-09-10 19:18:18 +0530

    Patterns  --  Pyramid.java

---
+++ Patterns/Pyramid.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        int row = 5;
+        for (int i = 1; i <= row; i++) {
+            for (int j=row-i;j>=0;j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
