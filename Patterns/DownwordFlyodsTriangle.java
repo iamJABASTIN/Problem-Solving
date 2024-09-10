76ab75e1be0cf3b3fc85cd15688af652f5d77721
JABASTIN <itsmejabastin@gmail.com>
2024-09-10 19:07:58 +0530

    Pattern  --  Downward Flyods Triangle

--- Patterns/DownwordFlyodsTriangle.java
+++ Patterns/DownwordFlyodsTriangle.java
@@ -4,8 +4,8 @@ public class DownwordFlyodsTriangle {
     public static void main(String[] args) {
         int row = 5;
 
-        for (int i = 0; i < row; i++) {
-            for (int j = row; j >= 0; j--) {
+        for (int i = 1; i <= row; i++) {
+            for (int j = row-i; j >= 0; j--) {
                 System.out.print("* ");
             }
             System.out.println();
