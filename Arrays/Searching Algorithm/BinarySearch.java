88f58a53c554111747e0c5e49bf7141afbe4be53
JABASTIN <itsmejabastin@gmail.com>
2024-10-11 19:39:06 +0530

    Updation

--- Arrays/Searching Algorithm/BinarySearch.java
+++ Arrays/Searching Algorithm/BinarySearch.java
@@ -17,7 +17,7 @@ public class BinarySearch {
         System.out.println("The element is present in the index "+ans);
     }
     static int BS(int[] arr, int target){
-        int start=0, end=arr.length;
+        int start=0, end=arr.length-1;
         while (start<=end) {
             int mid = start + (end - start) / 2;
             if(arr[mid] == target) return mid;
