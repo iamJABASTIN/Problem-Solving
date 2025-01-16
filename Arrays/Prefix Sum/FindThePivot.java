d335d692edff0e93c2d78186585d4bcde2057017
JABASTIN <itsmejabastin@gmail.com>
2025-01-16 20:32:32 +0530

    Arrays  --  Find The Pivot

---
+++ Arrays/Prefix Sum/FindThePivot.java
@@ -0,0 +1,42 @@
+/*
+ 724th problem in LEETCODE  --  Find Pivot Index
+
+Given an array of integers nums, calculate the pivot index of this array.
+
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+Return the leftmost pivot index. If no such index exists, return -1.
+
+Example 1:
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+ */
+
+
+public class FindThePivot {
+
+    static int findThePivot(int[] nums) {
+        int rs=0, ls=0;
+        for(int i : nums) {
+            rs += i;
+        }
+        for(int i=0;i<nums.length;i++) {
+            rs -= nums[i];
+            if(ls == rs) return i;
+            ls += nums[i];
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,7,3,6,5,6};
+        int ans = findThePivot(arr);
+        System.out.println("The output: "+ans);
+    }
+}
