0bac6dd641c3c465794d8aa833e5acb5dcd30b31
JABASTIN <itsmejabastin@gmail.com>
2025-04-05 21:12:57 +0530

    Update: Strings  --  Longest Substring Without Repeating Characters

--- Strings/LongestSubstringWithoutRepeatingCharacters.java
+++ Strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,9 +1,23 @@
+// 3rd problem in LEETCODE  --  Longest Substring Without Repeating Characters
 
-import java.util.HashSet;
+// Given a string s, find the length of the longest substring without duplicate characters.
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1
+import java.util.*;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
-        String str = "acddacbc";
+        String str = "abcabcbb";
         int right = 0, left = 0, maxLen = 0;
         HashSet<Character> seen = new HashSet<>();
         while(right < str.length()) {
@@ -15,7 +29,8 @@ public class LongestSubstringWithoutRepeatingCharacters {
                 seen.remove(str.charAt(left));
                 left++;
             }
-        }
-        System.out.println("The output is: "+maxLen);  
+        } 
+        
+        System.out.println("The output is: "+maxLen);
     }
 }
